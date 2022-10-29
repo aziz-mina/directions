9c47c280b3742a2797b47c34c358116f5d0bea79
mina issac <mina.issac.aziz@outlook.com>
2022-10-29 12:01:14 +0200

    'add_relation_to_posts'

--- app/Models/SavedPost.php
+++ app/Models/SavedPost.php
@@ -10,4 +10,9 @@ class SavedPost extends Model
     use HasFactory;
 
     protected $fillable = ['user_id', 'post_id'];
+
+    public function post()
+    {
+        return $this->belongsTo(Post::class);
+    }
 }
