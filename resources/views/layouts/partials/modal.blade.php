40927d9e1140236a158456cbcbb6c23094d4abcb
mina issac <mina.issac.aziz@outlook.com>
2022-10-29 09:18:08 +0200

    'fix_share_modal_bug'

--- resources/views/layouts/partials/modal.blade.php
+++ resources/views/layouts/partials/modal.blade.php
@@ -1,10 +1,12 @@
 <!-- Share Modal -->
 @isset($post)
-    <div class="modal fade " id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal fade "
+        @isset($index) id="{{ 'postModal-' . $index }}" @else  id='postModal' @endisset tabindex="-1"
+        aria-labelledby="postModalLabel" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Share Post</h5>
+                    <h5 class="modal-title" id="postModalLabel">Share Post</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
